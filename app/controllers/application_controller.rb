05186940d86821228ece7a439efbfb979dd67e9d
Ty Kujawa <comsenzce13@gmail.com>
2013-06-11 00:26:17 -0600

    recognize mobile browser -- basic jQuery Mobile added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 	before_filter :prepare_for_mobile
 	
+	#checks if user's brower is mobile
 	def is_mobile?
 		request.user_agent =~ /Mobile/
 	end
